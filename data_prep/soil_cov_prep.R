18790bd6560e71c5410068acc807a1506b7b585a
Allan Vikiru <vikiruallan12@gmail.com>
2024-02-23 19:05:14 +0300

    update: redefined covs extent. final modelling attempts

--- data_prep/soil_cov_prep.R
+++ data_prep/soil_cov_prep.R
@@ -1,5 +1,6 @@
 #set project directory
 proj_dir <- 'D:/r/KenyaDSM'
+epsg <- "epsg:4326"
 
 # covariate preparation and selection
 library(raster)
@@ -7,48 +8,50 @@ library(terra)
 library(sf)
 library(corrplot)
 library(psych)
-setwd(paste(proj_dir,'data/covariates_shp/train_cov', sep="/"))
+setwd(paste(proj_dir,'data/covariates_shp/soil_cov', sep="/"))
 # depth - 15-30 cm, 250m resolution
 covs_path <- getwd()
 
 # create raster stack
-epsg <- "epsg:4326"
 files <- list.files(path = covs_path, pattern = "tif*$", full.names = TRUE)
 covs <- stack(files)
-summary(covs)
+summary(covs) # it is normal to have NoData rasters check Fan et al. use location skipping
+plot(covs$clay) # null values are in the UG extent of the map (not covered in main covs)
 
-# clean rasters
-ph20 <- raster(paste(covs_path, "ph20.tif", sep="/"))
-occ <- raster(paste(covs_path, "occ.tif", sep="/"))
-# transform varying rasters in case of varying rasters error
-ph20 <- projectRaster(from = ph20, to = covs$annual_precip, method="ngb")
-occ <- projectRaster(from = occ, to = covs$annual_precip, method="ngb")
+# clean rasters: transform varying rasters in case of varying rasters error
+# cov1 <- raster(paste(covs_path, "ph20.tif", sep="/"))
+# cov2 <- raster(paste(covs_path, "occ.tif", sep="/"))
+# cov5 <- projectRaster(from = cov5, to = covs$annual_precip, method="ngb")
+# cov2 <- projectRaster(from = cov2, to = covs$annual_precip, method="ngb")
+# 
+# # clean rasters : fill rasters of NA values with 0 (avoid errors in kriging)
+cov2 <- raster(paste(covs_path, "h20cap_pf2_0.tif", sep="/"))
+cov3 <- raster(paste(covs_path, "h20cap_pf2_3.tif", sep="/"))
+cov4 <- raster(paste(covs_path, "h20cap_pf2_5.tif", sep="/"))
 
-# replace NA with 0s
-cov1 <- raster(paste(covs_path, "wet_index.tif", sep="/"))
-cov2 <- raster(paste(covs_path, "valley_depth.tif", sep="/"))
-cov1[is.na(cov2[])]<- 0
-cov1[is.na(cov2[])]<- 0
-#export cleaned rasters
-plot(cov1)
+cov2[is.na(cov2[])] <- 0
+cov3[is.na(cov3[])] <- 0
+cov4[is.na(cov4[])] <- 0
 summary(cov2)
-writeRaster(cov1, 'wet_index.tif', overwrite=TRUE)
-writeRaster(cov2, 'valley_depth.tif', overwrite=TRUE)
-remove(cov1)
-remove(cpv2)
+
+writeRaster(cov2, 'h20cap_pf2_0.tif', overwrite=TRUE)
+writeRaster(cov3, 'h20cap_pf2_3.tif', overwrite=TRUE)
+writeRaster(cov4, 'h20cap_pf2_5.tif',overwrite=TRUE)
+
+remove(cov4)
 
 #retry co-variate stacking
 files <- list.files(path = covs_path, pattern = "tif*$", full.names = TRUE)
 covs <- stack(files)
 # confirm selection
 summary(covs)
-plot(covs$occ)
+plot(covs$clay)
 
 # load soil training data for appending: convert to SPDF
-k_dat <- read.csv(paste(proj_dir,'data/soil_shp/model/k_model.csv', sep="/"))
-mg_dat <- read.csv(paste(proj_dir,'data/soil_shp/model/mg_model.csv', sep="/"))
-k_dat <- k_dat[, -4]
-mg_dat <- mg_dat[, -4]
+k_dat <- read.csv(paste(proj_dir,'data/soil_shp/soil_counties/soil_pts/k_pts/k_pts.csv', sep="/"))
+mg_dat <- read.csv(paste(proj_dir,'data/soil_shp/soil_counties/soil_pts/mg_pts/mg_pts.csv', sep="/"))
+k_dat <- k_dat[, -c(4:5)]
+mg_dat <- mg_dat[, -c(4:5)]
 coordinates(k_dat) <- ~ X + Y
 coordinates(mg_dat) <- ~ X + Y
 proj4string(k_dat) <- CRS(epsg)
@@ -63,24 +66,24 @@ k_covs <- as.data.frame(k_covs)
 mg_covs <- as.data.frame(mg_covs) 
 summary(k_covs)
 summary(mg_covs)
-k_covs <- k_covs[complete.cases(k_covs),] # remove nulls
+k_covs <- k_covs[complete.cases(k_covs),] # remove nulls (location skipping)
 mg_covs <- mg_covs[complete.cases(mg_covs),]
 
 #export cleaned covs data
 write.csv(k_covs, "k_full_covs.csv", row.names=FALSE)
-write.csv(k_covs, "mg_full_covs.csv", row.names=FALSE)
+write.csv(mg_covs, "mg_full_covs.csv", row.names=FALSE)
 
 # do correlation tests
-skew(k_covs[3:18], na.rm=TRUE) # check skewness to see variation from normal distribution
-skew(mg_covs[3:18], na.rm=TRUE)
+skew(k_covs[3:16], na.rm=TRUE) # check skewness to see variation from normal distribution
+skew(mg_covs[3:16], na.rm=TRUE)
 
-# large variations for most covs so apply spearman to select
-corrplot(cor(k_covs[3:18], method="spearman"), method="color") # do correlation plots
-corrplot(cor(mg_covs[3:18], method="spearman"), method="color")
+# small variations for most covs so apply pearson to select
+corrplot(cor(k_covs[3:20], method="pearson"), method="color") # do correlation plots
+corrplot(cor(mg_covs[3:20], method="pearson"), method="color")
 
 # as matrix format for selection
-k_cor <- abs(round(cor(x = as.matrix(k_covs[,3]), y = as.matrix(k_covs[,c(4:18)], method="spearman")),2)) # set to absolute values to capture both +ve and -ve correlations
-mg_cor <- abs(round(cor(x = as.matrix(mg_covs[,3]), y = as.matrix(mg_covs[,c(4:18)], method="spearman")),2))
+k_cor <- abs(round(cor(x = as.matrix(k_covs[,3]), y = as.matrix(k_covs[,c(4:20)], method="spearman")),2)) # set to absolute values to capture both +ve and -ve correlations
+mg_cor <- abs(round(cor(x = as.matrix(mg_covs[,3]), y = as.matrix(mg_covs[,c(4:20)], method="spearman")),2))
 row.names(k_cor) <- c('corr')
 row.names(mg_cor) <- c('corr')
 k_cor <- as.data.frame(k_cor[,k_cor["corr",] >= 0.3]) # select weak to strong correlations: check Mukaka, 2012
@@ -92,6 +95,48 @@ mg_list <- row.names(mg_cor) %>% c('X', 'Y', 'mg')
 k_dat <- subset(k_covs, select=k_list)
 mg_dat <- subset(mg_covs, select=mg_list)
 
-setwd(paste(proj_dir,'data/soil_shp/model', sep="/"))
-write.csv(k_dat, "k_model_cov.csv", row.names=FALSE)
-write.csv(mg_dat, "mg_model_cov.csv", row.names=FALSE)
+# remove h20_cap for modelling since they are similar 
+k_dat <- k_dat[, -c(4:5)]
+mg_dat <- mg_dat[, -c(4:5)]
+
+setwd(paste(proj_dir,'data/soil_shp/soil_counties/soil_pts', sep="/"))
+write.csv(k_dat, "k_pts/k_model_cov.csv", row.names=FALSE)
+write.csv(mg_dat, "mg_pts/mg_model_cov.csv", row.names=FALSE)
+
+# create and export test sets with selected covariates
+# k_test <- read.csv(paste(proj_dir,'data/soil_shp/k_test/k_test.csv', sep="/"))
+# mg_test <- read.csv(paste(proj_dir,'data/soil_shp/mg_test/mg_test.csv', sep="/"))
+# k_test <- k_test[, -4]
+# mg_test <- mg_test[, -4]
+# coordinates(k_test) <- ~ X + Y
+# coordinates(mg_test) <- ~ X + Y
+# proj4string(k_test) <- CRS(epsg)
+# proj4string(mg_test) <- CRS(epsg)
+
+
+# #select covariates and raster stack
+# setwd(paste(proj_dir,'data/covariates_shp/test_cov', sep="/"))
+# # depth - 15-30 cm, 250m resolution
+# covs_path <- getwd()
+# # create raster stack
+# files <- list.files(path = covs_path, pattern = "tif*$", full.names = TRUE)
+# covs <- stack(files)
+# summary(covs)
+# plot(covs$land_elevation)
+# # confirm that points lie within covariates
+# points(mg_test)
+# 
+# #  extract values from covariates
+# k_covs <- extract(x = covs, y = k_test, sp=TRUE)
+# mg_covs <- extract(x = covs, y = mg_test, sp=TRUE)
+# k_covs <- as.data.frame(k_covs) 
+# mg_covs <- as.data.frame(mg_covs) 
+# summary(k_covs)
+# summary(mg_covs)
+# k_covs <- k_covs[complete.cases(k_covs),] # remove nulls
+# mg_covs <- mg_covs[complete.cases(mg_covs),]
+# 
+# #export cleaned covs data
+# setwd(paste(proj_dir,'data/soil_shp/', sep="/"))
+# write.csv(k_dat, "k_test/k_model_cov.csv", row.names=FALSE)
+# write.csv(mg_dat, "mg_test/mg_model_cov.csv", row.names=FALSE)
--- map_model/k_mod.R
+++ map_model/k_mod.R
@@ -3,18 +3,24 @@ proj_dir <- 'D:/r/KenyaDSM'
 wsg84 <- "+init=epsg:4326"
 utm <- "+init=epsg:32236"
 
-setwd(paste(proj_dir,'data/soil_shp/model', sep="/"))
+setwd(paste(proj_dir,'data/soil_shp/soil_counties/soil_pts/k_pts', sep="/"))
 soil_path <- getwd()
 k_dat <- as.data.frame(read.csv("k_model_cov.csv", header = TRUE, sep = ","))
+# train_path <- getwd()
+# k_dat <- as.data.frame(read.csv("k_model_cov.csv", header = TRUE, sep = ","))
+# setwd(paste(proj_dir,'data/soil_shp/k_test', sep="/"))
+# test_path <- getwd()
+# k_test <- as.data.frame(read.csv("k_model_cov.csv", header = TRUE, sep = ","))
+# k_merge <- rbind(k_dat, k_test)
 
-# select covariates
+# select training covariates
 library(raster)
-setwd(paste(proj_dir,'data/covariates_shp/train_cov/', sep="/"))
+setwd(paste(proj_dir,'data/covariates_shp/soil_cov/', sep="/"))
 covs_path<- getwd()
-k_covs <-  names(k_dat[1:7])
+k_covs <-  names(k_dat[1:11])
 k_files <- paste0(covs_path,"/",k_covs, ".tif")
 k_covs <- stack(k_files)
-plot(k_covs$clay)
+plot(k_covs$occ)
 summary(k_covs)
 
 # data prep: convert to spatial format
@@ -24,29 +30,46 @@ k_dat@proj4string <- CRS(wsg84)
 points(k_dat)
 
 # EDA
-hist(k_dat$k) # right skewed
-#log transformation of targets for normal distribution
-hist(log(k_dat$k))
-k_df <- k_dat@data # prepare df
-k_df <- k_df[, c("k", names(k_covs))] # reorder columns
+k_train <- k_dat@data # prepare df
+k_train <- k_train[, c("k", names(k_covs))] # reorder columns
+# k_train <- k_train[(k_train$annual_precip & k_train$land_elevation & k_train$occ) != 0, ] # remove covs 0s for modelling
+hist(k_train$k) # right skewed
+hist(log(k_train$k)) #log transformation of targets for normal distribution
+
+k_eqn <- as.formula(log(k) ~ annual_precip+clay+h20cap_pf2_0+land_elevation+occ+ph20+silt+temp_annual_range+terrain_slope+tree_cover+wet_index)
 
 # run MLR
-k_mlr <- lm(log(k) ~ ., data = k_df)
-summary(k_mlr) # included more predictors at 0.2 correlation for K due to low MLR performance : 21.5 -> 30.6
+# k_merge <- k_merge[, -c(4:5)]
+library(caret)
+cv <- trainControl(method = "cv", number = 10) # 10 fold CV
+k_mlr <- train(k_eqn, data = k_train, method= "lm", trControl = cv)
+
+print(k_mlr) # R-squared  - 0.683 RMSE - 0.2593
+
+## TODO: PRINT MLR MODEL MAPS
 
 # transform CRS to projected coordinates for kriging
-k_train <- spTransform(k_train, CRS(utm))
+k_train <- spTransform(k_dat, CRS(utm))
 k_covs <- projectRaster(k_covs, crs = CRS(utm),method='ngb')
 k_covs <- as(k_covs, "SpatialGridDataFrame")
+k_train <- k_train[which(!duplicated(k_train@coords)),] #remove any duplicates
+
+
+# run OK
+library(automap)
+k_ok <- autoKrige.cv(log(k) ~ 1, k_train, model = c("Sph"))
+summary(ok)
+ok_mod <- lm(ok$krige_output@data$var1.pred ~ ok$krige_output@data$observed)
 
 # regression-kriging prediction.
-library(automap)
-# run universal Kriging function 
-k_krige <- autoKrige(formula =
-                       as.formula(k_mlr$call$formula),
+k_mlkr <- autoKrige.cv(formula = as.formula(k_eqn),
                      input_data = k_train,
-                     new_data = k_covs,
-                     verbose = TRUE)
+                     new_data = k_covs)
+
+summary(k_krige) # RMSE   0.08748
+
+k_mlkr_cv <- lm(k_krige$krige_output@data$var1.pred ~ kkrige$krige_output@data$observed)
+summary(k_mlkr_cv) # Adjusted R-squared:  0.9562 
 
 # transformed plots
 k_rkpred <- exp(raster(k_krige$krige_output[1])) # predicted K map
@@ -64,17 +87,3 @@ writeRaster(k_rkpredsd, filename = "k_train/rk_pred_sd.tif")
 setwd(paste(proj_dir,'map_model', sep="/"))
 saveRDS(k_mlr, file="models/k_mlr.Rds")
 saveRDS(k_krige, file="models/k_mlrk.Rds")
-
-# cross validation
-k_train <- k_train[which(!duplicated(k_train@coords)),] #remove any duplicates
-
-# MLRK with 10-fold CV
-kkrige_cv <- autoKrige.cv(formula =
-                            as.formula(k_mlr$call$formula),
-                          input_data = k_train,
-                          nfold=10)
-
-summary(kkrige_cv)
-
-mg_mlkr_cv <- lm(mgkrige_cv$krige.cv_output@data$var1.pred ~ mgkrige_cv$krige.cv_output@data$observed)
-summary(mg_mlkr_cv)
