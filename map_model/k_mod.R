83b9dda4fd64fb8d7a1a28c03fd904b08ddc2933
Allan Vikiru <vikiruallan12@gmail.com>
2024-03-10 21:50:04 +0300

    update: mg and k map generation

--- map_model/k_mod.R
+++ map_model/k_mod.R
@@ -3,189 +3,159 @@ proj_dir <- 'D:/r/KenyaDSM'
 wsg84 <- "+init=epsg:4326"
 utm <- "+init=epsg:32236"
 
+# select sample data - 1080 :: TODO - ADD NOISE AND SELECT FEATURES
 setwd(paste(proj_dir,'data/soil_shp/k_pts', sep="/"))
 soil_path <- getwd()
-k_dat <- as.data.frame(read.csv("k_subsample_model_cov.csv", header = TRUE, sep = ","))
+k_dat <- as.data.frame(read.csv("sample/k_sample_model.csv", header = TRUE, sep = ","))
+
+# convert to 70:30 training/testing, fill noise in training to avoid generalisation
+library(caret)
+part <- createDataPartition(k_dat$k, times = 1, p = 0.7, list = FALSE)
+k_train <- k_dat[part, ]
+k_test <- k_dat[-part, ]
+head(k_train)
+head(k_test)
+# write data
+write.csv(k_train, "sample/k_train.csv", row.names=FALSE)
+write.csv(k_test, "sample/k_test.csv", row.names=FALSE)
 
 # select training covariates
 library(raster)
 setwd(paste(proj_dir,'data/covariates_shp/soil_cov/', sep="/"))
 covs_path<- getwd()
-k_covs <-  names(k_dat[1:5])
+k_covs <-  names(k_dat[1:8])
 k_files <- paste0(covs_path,"/",k_covs, ".tif")
 k_covs <- stack(k_files)
-plot(k_covs$elevation)
 summary(k_covs)
+plot(k_covs$elevation)
 
 # data prep: convert to spatial format
 library(sp)
-coordinates(k_dat) <- ~ X + Y
-k_dat@proj4string <- CRS(wsg84)
-points(k_dat)
+coordinates(k_train) <- ~ X + Y
+k_train@proj4string <- CRS(wsg84)
+points(k_train)
+coordinates(k_test) <- ~ X + Y
+k_test@proj4string <- CRS(wsg84)
+points(k_test)
 
 # EDA
-# k_train <- k_dat@data # prepare df
-# k_train <- k_dat[, c("k", names(k_covs))] # reorder columns
-# k_train <- k_train[(k_train$annual_precip & k_train$land_elevation & k_train$occ) != 0, ] # remove covs 0s for modelling
 hist(k_dat$k) # right skewed
 hist(log(k_dat$k)) #log transformation of targets for normal distribution
-k_dat$log_k <- log(k_dat$k)
 
+# regression equation
+k_eqn <- as.formula(log(k) ~ annual_ppt+elevation+occ+ph20+slope+temp_annual_range+treecover+wetness_index)
+# cv <- trainControl(method = "cv", number = 5) # 5 fold CV (80% training, 20% testing)
 
-k_eqn <- as.formula(log_k ~ elevation+slope+temp_annual_range+treecover+wetness_index)
-# k_eqn <- as.formula(log_k ~ annual_precip+clay+h20cap_pf2_0)
+### run SVM
+library(e1071)
+k_svm <- train(k_eqn, data = k_train@data, method = 'svmRadial')
+print(k_svm) # RMSE - 0.165 R2 - 0.864, gamma = 0.222, C = 1
 
+# confirm tuned hyperparameters of gamma and cost in 5 fold cv
+# ct <- tune.control(cross = 5) # 5 fold CV (80% training, 20% testing)
+svm_tune <- tune(svm, 
+                 k_eqn, 
+                 data = k_train@data[,c("k",names(k_covs))],
+                 ranges = list(
+                   gamma = seq(0.2,0.5,0.05),
+                   cost = seq(0.1,1,0.1)))
+plot(svm_tune) # towards 0.2 gamma, 1 cost
+svm_tune$best.parameters
 
-# run MLR
-k_lm <- lm(k_eqn, k_dat)
-summary(k_lm) 
+# generate svm maps
+k_svmpred <- exp(predict(k_covs, k_svm))
+plot(k_svmpred)
 
-# run MLR-CV
-k_train <- k_dat@data # prepare df
-library(caret)
-cv <- trainControl(method = "cv", number = 5) # 5 fold CV
-k_mlr <- train(k_eqn, data = k_train, method= "lm", trControl = cv)
-print(k_mlr) # R-squared  - 0.66 RMSE - 0.25
+# export model and map
+setwd(paste(proj_dir,'map_model/models', sep="/"))
+model_path <- getwd()
+saveRDS(k_svm, file="k_svm.Rds")
 
-## TODO: PRINT MLR MODEL MAPS ##
+setwd(soil_path)
+writeRaster(k_svmpred, filename = "models/ksvm_pred.tif")
+
+### run MLR
+k_mlr <- train(k_eqn, data = k_train@data, method= "lm")
+print(k_mlr) # R-squared  - 0.657 RMSE - 0.261
+
+# generate MLR model maps
+k_mlrpred <- exp(predict(k_covs, k_mlr))
+plot(k_mlrpred)
+
+# export model and map
+setwd(model_path)
+saveRDS(k_mlr, file="k_mlr.Rds")
+
+setwd(soil_path)
+writeRaster(k_mlrpred, filename = "models/kmlr_pred.tif")
 
 # transform CRS to projected coordinates for kriging
-k_dat <- spTransform(k_dat, CRS(utm))
+k_train <- spTransform(k_train, CRS(utm))
 k_covs <- projectRaster(k_covs, crs = CRS(utm),method='bilinear')
 k_covs <- as(k_covs, "SpatialGridDataFrame")
-k_dat <- k_dat[which(!duplicated(k_dat@coords)),] #remove any duplicates
+k_train <- k_train[which(!duplicated(k_train@coords)),] #remove any duplicates
 
-## run IDW
-library(gstat)
-k_idw <- krige(log_k ~ 1, k_dat, k_covs)
-summary(k_idw)
-
-k_idw.na <- na.omit(k_idw)
-idw.pred <- rasterFromXYZ(as.data.frame(k_idw)[, c("s1", "s2", "var1.pred")])
-plot(idw.pred)
-
+#### run OK
 library(automap)
-k_idw.cv <- autoKrige.cv(log_k ~ 1, k_dat)
-idw_mod <- lm(k_idw.cv$krige.cv_output@data$var1.pred ~ k_idw.cv$krige.cv_output@data$observed)
-
-#####
-# run OK
-library(gstat)
-library(automap)
-
-k_autovgm <- formula(log_k ~ 1, k_dat, model="Sph")
-
-# variogram: closer things are more predictable and have less variability
-# set K to variogram model - for each pair of points, the gamma value/semivariance between them is plotted against the distance between them
-
-k_exp_vgm <- variogram(log_k ~ 1, k_dat)
-plot(k_exp_vgm) # experimental variogram
-
-# nugget: start of points at x; range: point at y that flats out
-# sill : point at x that flats out; p-sill: sill - nugget
-
-# theoretical variogram
-k_th_vgm <- vgm(psill = 0.147, model="Lin", range=39500, nugget = 0.008) # N - 0.02, R: 38000, S: 0.07 PS: 0.055 
-plot(k_exp_vgm, k_th_vgm)
-
-# fit theoretical and experimental variograms
-k_fit_vgm <- fit.variogram(k_exp_vgm, k_th_vgm)
-plot(k_fit_vgm)
-
-#ordinary kriging
-k_ok <- krige(log_k ~ 1, 
-              locations = k_dat,
-              newdata = k_covs, 
-              model= k_fit_vgm,
-              beta = 0)
-
-plot(k_ok["var1.pred"], main = "Log Soil Potassium (OK)")
-k_ok$pred_k <- exp(k_ok$var1.pred)
-plot(k_ok["pred_k"], main = "Predicted Soil Potassium (OK)")
-
-#####
-library(gstat)
-library(automap)
-k_dat$residuals <- residuals(k_lm) # fit residuals
-k_autovgm <- formula(residuals ~ 1, k_dat, model="Sph")
-
-# variogram: closer things are more predictable and have less variability
-# set residuals to variogram model - for each pair of points, the gamma value/semivariance between them is plotted against the distance between them
-
-k_resid_var <- variogram(residuals ~ 1, data = k_dat)
-plot(k_resid_var) # experimental variogram
-
-# nugget: start of points at x; range: point at y that flats out
-# sill : point at x that flats out; p-sill: sill - nugget
-
-#theoretical variogram
-k_resid_vgm <- vgm(psill = 0.056, model = "Sph", range=38000, nugget = 0.014) # N - 0.02, R: 38000, S: 0.07 PS: 0.055 
-plot(k_resid_var, k_resid_vgm)
-
-# fit theoretical and experimental variograms
-kr_vgm <- fit.variogram(k_resid_var, k_resid_vgm)
-plot(k_resid_var, kr_vgm)
-
-#ordinary kriging
-k_ok <- krige(residuals ~ 1, 
-              locations = k_dat,
-              newdata = k_covs, 
-              model= kr_vgm,
-              beta = 0)
-
-plot(k_ok["var1.pred"], main = "Log Soil Potassium (OK)")
-k_ok$pred_k <- exp(k_ok$var1.pred)
-plot(k_ok["pred_k"], main = "Predicted Soil Potassium (OK)")
-
-
-
-
-library(automap)
-k_ok <- autoKrige(log_k ~ 1, 
-                  input_data = k_dat, 
+k_ok <- autoKrige(log(k) ~ 1, 
+                  input_data = k_train, 
                   verbose = TRUE)
 plot(k_ok)
-ok_mod <- lm(k_ok$krige_output@data$var1.pred ~ k_ok$krige_output@data$observed)
+summary(k_ok)
+
+# 5 fold cv of OK model
+# k_ok_cv <- autoKrige.cv(formula = log_k ~ 1,
+#                         input_data = k_train,
+#                         verbose = c(interactive(), interactive()), 
+#                         nfold = 5)
+# summary(k_ok_cv) # RMSE 0.111
+# k_ok_mod <- lm(k_ok$krige.cv_output@data$var1.pred ~ k_ok$krige_output@data$observed)
+# summary(k_ok_mod) # R2 = 0.936
+
+# generate OK maps
+k_okpred <- exp(raster(k_ok$krige_output[1])) # predicted K map
+# k_okpredsd <- exp(raster(k_ok$krige_output[3])) # st error of predictions
+
+k_okpred <- projectRaster(k_okpred, crs = wsg84)
+# k_okpredsd <- projectRaster(k_okpredsd, crs = wsg84)
+
+plot(k_okpred)
+# plot(k_okpredsd)
+
+# export model and map
+setwd(model_path)
+saveRDS(k_ok, file="k_ok.Rds")
+
+setwd(soil_path)
+writeRaster(k_okpred, filename = "models/kok_pred.tif")
 
 # run MLRK
 k_mlrk <- autoKrige(formula = as.formula(k_eqn), 
-                  input_data = k_dat, 
-                  new_data = k_covs,
-                  verbose = TRUE)
+                    input_data = k_train, 
+                    new_data = k_covs,
+                    verbose = TRUE)
 plot(k_mlrk)
-## TODO: convert plotted maps
 
+# k_mlrk_cv <- autoKrige.cv(formula = as.formula(k_eqn),
+#                        input_data = k_train, 
+#                        verbose = c(interactive(), interactive()), 
+#                        nfold = 5)
+# summary(k_mlrk_cv) # RMSE 0.1075
+# 
+# k_mlkr_mod <- lm(k_mlrk_cv$krige.cv_output@data$var1.pred ~ k_mlrk_cv$krige.cv_output@data$observed)
+# summary(k_mlkr_mod) # R2 = 0.941
 
-# 10 fold cv of OK model
-k_ok_cv <- autoKrige.cv(formula = k ~ 1,
-                       input_data = k_dat,
-                       verbose = c(interactive(), interactive()), 
-                       nfold = 10)
+# generate MLRK maps
+k_mlrkpred <- exp(raster(k_mlrk$krige_output[1])) # predicted K map
+# k_mlrkpredsd <- exp(raster(k_mlrk$krige_output[3])) # st error of predictions
 
-ok_mod <- lm(k_ok_cv$krige.cv_output@data$var1.pred ~ k_ok_cv$krige.cv_output@data$observed)
-summary(ok_mod) # R2 = 0.939
+k_mlrkpred <- projectRaster(k_mlrkpred, crs = wsg84)
 
-k_mlrk_cv <- autoKrige.cv(formula = as.formula(k_eqn),
-                       input_data = k_dat, 
-                       verbose = c(interactive(), interactive()), 
-                       nfold = 10)
-summary(k_ok_cv)
-mlkr_mod <- lm(k_mlrk_cv$krige.cv_output@data$var1.pred ~ k_mlrk_cv$krige.cv_output@data$observed)
-summary(mlkr_mod) # R2 = 0.944
+plot(k_mlrkpred)
 
-# transformed plots
-k_okpred <- exp(raster(k_ok_cv$krige.cv_output[1])) # predicted K map
-k_okpredsd <- exp(raster(k_ok_cv$krige.cv_output[3])) # st error of predictions
-
-# plot and export maps
-plot(k_okpred)
-plot(k_okpredsd)
+# export model and map
+setwd(model_path)
+saveRDS(k_mlrk, file="k_mlrk.Rds")
 
 setwd(soil_path)
-writeRaster(k_rkpred, filename = "k_train/rk_pred.tif")
-writeRaster(k_rkpredsd, filename = "k_train/rk_pred_sd.tif")
-
-#export models
-setwd(paste(proj_dir,'map_model', sep="/"))
-saveRDS(k_mlr, file="models/k_mlr.Rds")
-saveRDS(k_krige, file="models/k_mlrk.Rds")
+writeRaster(k_mlrkpred, filename = "models/kmlrk_pred.tif")
--- map_model/ok.R
+++ map_model/ok.R
@@ -1,32 +1,67 @@
-# REFER TO RK FOR PREP
-## OK ATTEMPT
+# run OK
+library(gstat)
 library(automap)
-k_autovgm <- formula(residuals ~ 1, k_train, model="Sph")
 
-k_train$residuals <- residuals(k_mlr) # fit residuals
-mg_train$residuals <- residuals(mg_mlr)
+k_autovgm <- formula(log_k ~ 1, k_dat, model="Sph")
+
+# variogram: closer things are more predictable and have less variability
+# set K to variogram model - for each pair of points, the gamma value/semivariance between them is plotted against the distance between them
+
+k_exp_vgm <- variogram(log_k ~ 1, k_dat)
+plot(k_exp_vgm) # experimental variogram
+
+# nugget: start of points at x; range: point at y that flats out
+# sill : point at x that flats out; p-sill: sill - nugget
+
+# theoretical variogram
+k_th_vgm <- vgm(psill = 0.147, model="Lin", range=39500, nugget = 0.008) # N - 0.02, R: 38000, S: 0.07 PS: 0.055 
+plot(k_exp_vgm, k_th_vgm)
+
+# fit theoretical and experimental variograms
+k_fit_vgm <- fit.variogram(k_exp_vgm, k_th_vgm)
+plot(k_fit_vgm)
+
+#ordinary kriging
+k_ok <- krige(log_k ~ 1, 
+              locations = k_dat,
+              newdata = k_covs, 
+              model= k_fit_vgm,
+              beta = 0)
+
+plot(k_ok["var1.pred"], main = "Log Soil Potassium (OK)")
+k_ok$pred_k <- exp(k_ok$var1.pred)
+plot(k_ok["pred_k"], main = "Predicted Soil Potassium (OK)")
+
+#####
+library(gstat)
+library(automap)
+k_dat$residuals <- residuals(k_lm) # fit residuals
+k_autovgm <- formula(residuals ~ 1, k_dat, model="Sph")
 
 # variogram: closer things are more predictable and have less variability
 # set residuals to variogram model - for each pair of points, the gamma value/semivariance between them is plotted against the distance between them
-k_resid_var <- variogram(residuals ~ 1, k_train)
-mg_resid_var <- variogram(residuals ~1, mg_train)
+
+k_resid_var <- variogram(residuals ~ 1, data = k_dat)
 plot(k_resid_var) # experimental variogram
-plot(mg_resid_var)
+
 # nugget: start of points at x; range: point at y that flats out
 # sill : point at x that flats out; p-sill: sill - nugget
 
 #theoretical variogram
-k_resid_vgm <- vgm(psill = 0.03, model = "Sph", range=23000, nugget = 0.01) # N - 0.01, R: 23000, S: 0.04 PS: 0.03 
+k_resid_vgm <- vgm(psill = 0.056, model = "Sph", range=38000, nugget = 0.014) # N - 0.02, R: 38000, S: 0.07 PS: 0.055 
 plot(k_resid_var, k_resid_vgm)
-
-mg_resid_vgm <- vgm(psill = 0.016, model = "Sph", range=28000, nugget = 0.014) # N - 0.014, R: 28000, S: 0.03 PS: 0.016 
-plot(mg_resid_var, mg_resid_vgm)
 
 # fit theoretical and experimental variograms
 kr_vgm <- fit.variogram(k_resid_var, k_resid_vgm)
 plot(k_resid_var, kr_vgm)
-mgr_vgm <- fit.variogram(mg_resid_var, mg_resid_vgm)
-plot(mg_resid_var, mgr_vgm)
 
 #ordinary kriging
-kr_sk <- krige(residuals ~ 1, k_train, newdata = k_covs, model= kr_vgm)
+k_ok <- krige(residuals ~ 1, 
+              locations = k_dat,
+              newdata = k_covs, 
+              model= kr_vgm,
+              beta = 0)
+
+plot(k_ok["var1.pred"], main = "Log Soil Potassium (OK)")
+k_ok$pred_k <- exp(k_ok$var1.pred)
+plot(k_ok["pred_k"], main = "Predicted Soil Potassium (OK)")
