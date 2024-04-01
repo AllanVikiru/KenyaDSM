5cd281f9612cf51174825bc2cb804989bd2f36e2
Allan Vikiru <vikiruallan12@gmail.com>
2024-04-01 04:06:32 +0300

    cleaned app structure

--- app/application.py
+++
@@ -1,4 +0,0 @@
-from recommender import create_app
-
-application = create_app()
-
--- app/recommender/routes.py
+++ app/recommender/routes.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request, send_from_directory
+    Blueprint, render_template, request
 )
 from .src.layoutUtils import *
 from .src.auth import *
@@ -24,12 +24,3 @@ def index():
 # def generate():
 #     foods = read_map()
 #     return render_template('forms/generator.html', foods=foods)
-
-#MANAGE sitemap and robots calls 
-#These files are usually in root, but for Flask projects must
-#be in the static folder
-@bp.route('/robots.txt')
-@bp.route('/sitemap.xml')
-def static_from_root():
-    return send_from_directory(current_app.static_folder, request.path[1:])
-
