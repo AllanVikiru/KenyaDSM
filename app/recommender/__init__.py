1d56291788a9c91e142023847f4dd0221f5c0d6a
Allan Vikiru <vikiruallan12@gmail.com>
2024-05-14 06:23:42 +0300

    update: for defence demo

--- app/recommender/__init__.py
+++ app/recommender/__init__.py
@@ -1,17 +1,15 @@
 import os
 
 from flask import Flask
+from .src import db_connect
+from . import routes
 
 def create_app(): # create and configure app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.environ['SESSION_SECRET'],
     )
-
-    from .src import db_connect
     db_connect.init_app(app)
-
-    from . import routes
     app.register_blueprint(routes.bp)
 
     return app
